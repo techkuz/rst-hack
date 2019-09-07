73ece5ee3e9ced6097f1fc5aa3e0dcd373273a60
Kuzma Leshakov <leshakov.kuzma@clover.global>
2019-09-07 19:36:10 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import json
+
+
+class Courier:
+    courier_id = None
+    location_x = None
+    location_y = None
+    status = 'FREE'
+
+
+class Depot:
+    point_id = None
+    location_x = None
+    location_y = None
+
+
+class Order:
+    order_id = None
+    pickup_point_id = None
+    pickup_location_x = None
+    pickup_location_y = None
+    pickup_from = None
+    pickup_to = None
+    dropoff_point_id = None
+    dropoff_location_x = None
+    dropoff_location_y = None
+    dropoff_from = None
+    dropoff_to = None
+    payment = None
+
+
+def init_start(file):
+    with open(FILE, 'r') as file:
+        data = json.load(file)
+        # print(data)
+        couriers = []
+        for courier in data['couriers']:
+            new_courier = Courier()
+            new_courier.courier_id = courier.get('courier_id')
+            new_courier.location_x = courier.get('location_x')
+            new_courier.location_y = courier.get('location_y')
+            couriers.append(new_courier)
+
+        depots = []
+        for depot in data['depots']:
+            new_depot = Depot()
+            new_depot.point_id = depot.get('point_id')
+            new_depot.location_x = depot.get('location_x')
+            new_depot.location_y = depot.get('location_y')
+            depots.append(new_depot)
+
+        orders = []
+        for order in data['orders']:
+            new_order = Order()
+            new_order.order_id = order.get('order_id')
+            new_order.pickup_point_id = order.get('pickup_point_id')
+            new_order.pickup_location_x = order.get('pickup_location_x')
+            new_order.pickup_location_y = order.get('pickup_location_y')
+            new_order.pickup_from = order.get('pickup_from')
+            new_order.pickup_to = order.get('pickup_to')
+            new_order.dropoff_point_id = order.get('dropoff_point_id')
+            new_order.dropoff_location_x = order.get('dropoff_location_x')
+            new_order.dropoff_location_y = order.get('dropoff_location_y')
+            new_order.dropoff_from = order.get('dropoff_from')
+            new_order.dropoff_to = order.get('dropoff_to')
+            new_order.payment = order.get('payment')
+            orders.append(new_order)
+
+        res = {'couriers': couriers, 'depots': depots, 'orders': orders}
+        return res
+
+
+FILE = 'contest_input.json'
+
+if __name__ == '__main__':
+    initial_data = init_start(file=FILE)
+    couriers = initial_data.get('couriers')
+    depots = initial_data.get('depots')
+    orders = initial_data.get('orders')
+
+    current_time = 0
